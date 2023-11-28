39f80dfe6dbf0e29af4d89c21d3cd43a6ea07103
jun <ggprgrkjh@naver.com>
2023-11-28 23:58:19 +0900

    feat(inputview): 당첨 번호 입력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,5 +10,11 @@ public class InputView {
         return input;
     }
 
+    public static String readWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return input;
+    }
+
 
 }
