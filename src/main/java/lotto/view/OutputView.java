b121d0dd4ac0ea0f7a8992e5686e188c2b5d7fe0
jun <ggprgrkjh@naver.com>
2023-11-29 00:44:00 +0900

    feat(outputview): 당첨 결과를 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -19,6 +19,11 @@ public class OutputView {
 
 
     public static void printResult(Map<LottoRank, Integer> result) {
-
+        System.out.println("당첨 통계\n---");
+        System.out.println("3개 일치 (5,000원) - " + result.get(LottoRank.FIFTH) + "개");
+        System.out.println("4개 일치 (50,000원) - " + result.get(LottoRank.FOURTH)+ "개");
+        System.out.println("5개 일치 (1,500,000원) - " + result.get(LottoRank.THIRD)+ "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result.get(LottoRank.SECOND)+ "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + result.get(LottoRank.FIRST)+ "개");
     }
 }
--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -61,8 +61,13 @@ public class OutputViewTest {
         //when
         OutputView.printResult(LottoResult.getResult());
         //then
-        assertThat(output.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]\n[5, 6, 7, 8, 9, 10]\n");
+        assertThat(output.toString()).isEqualTo(
+                "당첨 통계\n"
+                        + "---\n"
+                        + "3개 일치 (5,000원) - 1개\n"
+                        + "4개 일치 (50,000원) - 0개\n"
+                        + "5개 일치 (1,500,000원) - 0개\n"
+                        + "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n"
+                        + "6개 일치 (2,000,000,000원) - 1개\n");
     }
-
-
 }
