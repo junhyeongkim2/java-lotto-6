a235a6e30e0eca68b5e45e1adbd2616f766b74dd
jun <ggprgrkjh@naver.com>
2023-11-28 17:13:13 +0900

    feat(winningnumbers): 당첨 번호와 보너스 번호를 생성 기능 추가

---
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -0,0 +1,40 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class WinningNumbers {
+    private final List<Integer> winningNumbers;
+    private int bonusNumber;
+
+    private WinningNumbers(List<Integer> winningNumbers) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = createBonusNumber();
+    }
+
+    public static WinningNumbers create() {
+        return new WinningNumbers(createRandomNumbers());
+    }
+
+    public static List<Integer> createRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    public int createBonusNumber() {
+        while (true) {
+            int uniqueBonusNumber = Randoms.pickNumberInRange(1, 45);
+            if (!winningNumbers.contains(uniqueBonusNumber)) {
+                return uniqueBonusNumber;
+            }
+        }
+    }
+
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
---
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -0,0 +1,29 @@
+package lotto.model;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class WinningNumbersTest {
+
+    @DisplayName("당첨번호 및 보너스번호 생성 테스트")
+    @Test
+    void createWinningNumbers_EqualNumbers_Success() {
+        //given
+        WinningNumbers winningNumbers = WinningNumbers.create();
+        //when
+        List<Integer> numbers = winningNumbers.getWinningNumbers();
+        int bonusNumber = winningNumbers.getBonusNumber();
+        //then
+        assertThat(numbers.size()).isEqualTo(6);
+        assertThat(numbers).allSatisfy(value -> assertThat(value).isBetween(1, 45));
+        assertThat(numbers.contains(bonusNumber)).isFalse();
+        assertThat(bonusNumber).isBetween(1,45);
+
+    }
+
+
+}
