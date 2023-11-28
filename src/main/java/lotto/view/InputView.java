81b5c724d42978ff5f6c14b676d312395f11f58b
jun <ggprgrkjh@naver.com>
2023-11-29 02:43:01 +0900

    feat(inputview): 구입금액 숫자 아닌 값 입력될 경우 에러 발생

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,12 +1,17 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class InputView {
+
+    private static final String BUYAMOUBT_PATTERN = "^[0-9]+";
+
 
     public static String readBuyAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
+        validateBuyAmount(input);
         System.out.println();
         return input;
     }
@@ -22,6 +27,12 @@ public class InputView {
         System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
         return input;
+    }
+
+    public static void validateBuyAmount(String input) {
+        if (!Pattern.matches(BUYAMOUBT_PATTERN, input)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 구입금액이 입력되었습니다! 다시 입력해 주세요.");
+        }
     }
 
 
